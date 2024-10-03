5936ca0f961813b24085352b09263349d24a35d4
Vanessa Reteguín <145515264+vcardui@users.noreply.github.com>
2024-10-02 19:08:11 -0600

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,25 +1,30 @@
 from turtle import Turtle
 
 ALIGNMENT = 'center'
-FONT = ('Courier', 20, 'normal')
+FONT = ('Courier', 24, 'normal')
 
 class Scoreboard(Turtle):
+
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.hideturtle()
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
+        self.color("white")
         self.penup()
-        self.speed("fastest")
-        self.sety(270)
-        self.pencolor("white")
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.goto(0,270)
+        self.hideturtle()
+        self.update_scoreboard()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def levelUP(self):
+    def update_scoreboard(self):
         self.clear()
-        self.score += 1
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
-
+    def reset(self):
+        if self.high_score < self.score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.score}")
+        self.score = 0
+        self.update_scoreboard()
